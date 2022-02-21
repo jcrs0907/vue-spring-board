8bb0431d11bdd37b68b8b6f3860f1ec3c7f6911e
CHAERIN\chael <jcrs0907@gmail.com>
2022-02-21 11:38:11 +0900

    2022.02.21 add init

---
+++ src/main/java/com/board/vuespringboard/VueSpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.vuespringboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringBoardApplication.class, args);
+	}
+
+}
